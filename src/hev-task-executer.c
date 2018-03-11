73868eadd00acf657ce14e336694e3afa4fbf868
Heiher <r@hev.cc>
2018-03-11 23:57:35 +0800

    HevTask: Move stack overflow detection to task executer.

--- src/hev-task-executer.c
+++ src/hev-task-executer.c
@@ -7,8 +7,12 @@
  ============================================================================
  */
 
+#include <assert.h>
+
 #include "hev-task-executer.h"
 #include "hev-task-system-private.h"
+
+#define STACK_OVERFLOW_DETECTION_TAG	(0xdeadbeefu)
 
 void
 hev_task_executer (HevTask *task)
@@ -16,7 +20,15 @@ hev_task_executer (HevTask *task)
 	if (setjmp (task->context) == 0)
 		return;
 
+#ifdef ENABLE_STACK_OVERFLOW_DETECTION
+	*(unsigned int *) task->stack = STACK_OVERFLOW_DETECTION_TAG;
+#endif
+
 	task->entry (task->data);
+
+#ifdef ENABLE_STACK_OVERFLOW_DETECTION
+	assert (*(unsigned int *) task->stack == STACK_OVERFLOW_DETECTION_TAG);
+#endif
 
 	hev_task_system_kill_current_task ();
 }
